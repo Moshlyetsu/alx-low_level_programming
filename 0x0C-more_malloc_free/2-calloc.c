473ed314f55ba1851cd079c8718d2114f88539de
Moshlyetsu <mmanicuz@yahoo.com>
2023-10-05 16:45:03 -0700

    0x0C. C - More malloc, free Tasks updated

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,27 +2,6 @@
 #include "main.h"
 
 /**
- * *_memset -> must fill memory with a constant byte.
- * @s: the memory area that must be filled.
- * @b: the character  to be copied.
- * @n: the number of times that b must be copied.
- * Return: the pointer to the memory area s.
- */
- 
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int x;
-
-	for (x = 0; x < n; x++)
-	{
-		s[x] = b;
-	}
-	_putchar('\n');
-
-	return (s);
-}
-
-/**
  * *_calloc -> must allocate memory for an array.
  * @nmemb: the number of elements in the array.
  * @size: the size of each element.
@@ -31,21 +10,22 @@ char *_memset(char *s, char b, unsigned int n)
  
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *pntr;
+	void *memory;
+	char *populate;
+	unsigned int pos = 0;
 
 	if (nmemb == 0 || size == 0)
-	{
 		return (NULL);
-	}
-	pntr = malloc(size * nmemb);
 
-	if (pntr == NULL)
-	{
+	memory = malloc(size * nmemb);
+
+	if (memory == NULL)
 		return (NULL);
-	}
-	_memset(pntr, 0, nmemb * size);
 
-	_putchar('\n');
+	populate = memory;
 
-	return (pntr);
+	for (; pos < (size * nmemb); pos++)
+		populate[pos] = '\0';
+
+	return (memory);
 }
