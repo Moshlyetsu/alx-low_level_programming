4ef48805b3fb2ba3b01a1aeb2fc7658496a27c2b
Moshlyetsu <mmanicuz@yahoo.com>
2023-10-19 19:24:22 -0700

    0-print_list.c corrections

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,12 +3,12 @@
 
 /**
  * print_list ->MUST  print all the elements of a linked list.
- * @h: the pointer to the list_t list to be printed. 
+ * @h: the pointer to the list_t list to be printed.
  * Return: must be number of nodes found in the list_t.
  */
 size_t print_list(const list_t *h)
 {
- 
+
 	size_t node_count = 0;
 
 	while (h)
@@ -18,10 +18,10 @@ size_t print_list(const list_t *h)
 
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		
+
 		node_count++;
-		h = h->next;	
+		h = h->next;
 	}
 
- return (node_count);
+	return (node_count);
 }
