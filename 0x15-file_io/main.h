482ea9325375f8c2e6979b789e1259f793b5bb6d
Moshlyetsu <mmanicuz@yahoo.com>
2023-10-31 18:00:49 -0700

    corrections

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
 /*
  * Desc: Main Header file containing prototypes for all functions.
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
